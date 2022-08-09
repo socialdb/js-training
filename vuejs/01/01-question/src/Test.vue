725a66d609ee96d464aa6fddefa9aa0f29c92ec0
shunsuke0907 <if.0907@i.softbank.jp>
2022-08-09 16:10:19 +0900

    add: 交流会用

---
+++ vuejs/01/01-question/src/Test.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="main-block full box">
+    <div class="editor half">
+      <div>名字: 山田</div>
+      <div>名前: 太郎</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      lastName: "",
+      firstName: "",
+    };
+  },
+}
+</script>
+
+<style scoped>
+.main-block {
+  width: 100%;
+  position: relative;
+  padding: 15px;
+}
+
+.full {
+  width: 100%;
+  position: relative;
+
+}
+
+.half {
+  width: 40%;
+  position: relative;
+}
+
+.box {
+  display:flex;
+  justify-content:center;
+  align-items:center;
+}
+
+.preview {
+  background-color: #f5f5f5;
+  border: 1px solid #ccc;
+  height: 200px;
+  padding: 15px;
+}
+</style>
--- vuejs/01/01-question/src/main.js
+++ vuejs/01/01-question/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Test from './Test.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+createApp(Test).mount('#test')
